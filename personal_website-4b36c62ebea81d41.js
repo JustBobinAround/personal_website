3d16125fab75cdd989a1c4fb52584c12a5b34837
justbobinaround <roberthenryjunkins@gmail.com>
2023-05-30 19:45:34 -0400

    modified:   Makefile 	new file:   assets/404.html 	new file:   dist/404.html 	modified:   dist/index.html 	renamed:    dist/personal_website-889e4715ec9f97d.js -> dist/personal_website-4b36c62ebea81d41.js 	renamed:    dist/personal_website-889e4715ec9f97d_bg.wasm -> dist/personal_website-4b36c62ebea81d41_bg.wasm 	modified:   index.html 	modified:   src/main.rs

--- personal_website-4b36c62ebea81d41.js
+++ personal_website-4b36c62ebea81d41.js
@@ -827,7 +827,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('personal_website-889e4715ec9f97d_bg.wasm', import.meta.url);
+        input = new URL('personal_website-4b36c62ebea81d41_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
