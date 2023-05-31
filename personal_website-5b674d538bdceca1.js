2baf4add6db8969d5c1cf14b63c7deee7b712a6a
justbobinaround <roberthenryjunkins@gmail.com>
2023-05-30 20:30:56 -0400

    modified:   Makefile 	modified:   dist/index.html 	renamed:    dist/personal_website-9e9f15b0cf75f088.js -> dist/personal_website-5b674d538bdceca1.js 	renamed:    dist/personal_website-9e9f15b0cf75f088_bg.wasm -> dist/personal_website-5b674d538bdceca1_bg.wasm 	modified:   src/components/resume.rs

--- personal_website-5b674d538bdceca1.js
+++ personal_website-5b674d538bdceca1.js
@@ -836,7 +836,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('personal_website-9e9f15b0cf75f088_bg.wasm', import.meta.url);
+        input = new URL('personal_website-5b674d538bdceca1_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
